0509d4de630f903606dfb4e5d35abcc5665bb7bf
Manprit312 <manprit.kaur@appsmartz.com>
2024-03-14 16:01:53 +0530

    branch

---
+++ src/assets/brand/toggler.js
@@ -0,0 +1,10 @@
+// togg.js
+import React from 'react';
+
+const TogglerIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7 11" width="7" height="11">
+    <path d="M1.44165 9.71191L5.83755 5.11017L1.23581 0.714268" stroke="#637381" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"/>
+  </svg>
+);
+
+export default TogglerIcon;
